c1004fe1a7ae81c596b17c5a694fcbd84e975a11
Joseph Szczesniak <nukapunk@gmail.com>
2016-01-11 13:56:46 -0600

    Added route params example.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,23 +1,25 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider){
+app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: '/pages/home.html',
     controller: 'homeCtrl'
   }).when('/about', {
     templateUrl: '/pages/about.html',
     controller: 'aboutCtrl'
+  }).when('/users/:id', {
+    templateUrl: '/pages/users.html',
+    controller: 'usersCtrl'
   });
 }]);
 
 
 app.controller('homeCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.message = "Welcome to the home page!";
-
   $http({
     url: '/users',
     method: 'get'
-  }).then(function(response){
+  }).then(function (response) {
     $scope.users = response.data;
   });
 
@@ -25,5 +27,28 @@ app.controller('homeCtrl', ['$scope', '$http', function ($scope, $http) {
 
 app.controller('aboutCtrl', ['$scope', function ($scope) {
   $scope.message = "Welcome to the about page!";
+
+}]);
+
+
+app.controller('usersCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+
+  // Given:
+  // URL: http://localhost:3000/#/Chapter/1/Section/2?search=moby
+  // Route: /Chapter/:chapterId/Section/:sectionId
+  //
+  // Then $routeParams ==> {chapterId:1, sectionId:2, search:'moby'}
+
+  console.log($routeParams);
+
+  $scope.message = "Welcome to the user page! You are looking at user id " + $routeParams.id;
+
+  // go get that one
+  $http({
+    url: '/users/' +  $routeParams.id,
+    method: 'get'
+  }).then(function (response) {
+    $scope.user = response.data;
+  });
 
 }]);
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+// replace with db data
+var users = [{id: 1, name: 'Joseph'}, {id: 2, name: 'Antoinette'}, {id: 3, name: 'Joel'}, {id: 4, name: 'Kristy'}
+  , {id: 5, name: 'Scott'}, {id: 6, name: 'Ryan'}, {id: 7, name: 'Kris'}];
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  // replace with mongo data from users collection
-  var users = [{name: 'Joseph'}, {name: 'Antoinette'}];
   res.json(users);
+});
+
+/* GET single user */
+router.get('/:id', function (req, res, next) {
+
+  // this is how you get data from a url, example: http://localhost:3000/users/2
+  var id = req.params.id;
+
+  res.json(users[id]);
 });
 
 module.exports = router;
